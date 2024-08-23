97f00bc397b82995f399105b92c3d84360b94214
Waqas Liaqat <waqasliaqat4444@gmail.com>
2024-08-23 11:54:04 +0500

    laravel mix and tailwind cc installed for this project specificially.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
